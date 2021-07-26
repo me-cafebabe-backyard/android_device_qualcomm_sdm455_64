250cd8ae7d829296edfef76c95c4830fbf169e7a
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-07-26 14:23:17 +0800

    TEMP: sdm455_64: SELinux Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,7 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 \
     lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 \
     service_locator.enable=1 loop.max_part=7 swiotlb=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
