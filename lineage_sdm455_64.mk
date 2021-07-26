69ab3ea6199260477bdb268f570d014defad39b8
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-07-26 14:09:38 +0800

    sdm455_64: Inherit 4096 Dalvik Heap makefile

--- lineage_sdm455_64.mk
+++ lineage_sdm455_64.mk
@@ -17,6 +17,7 @@
 # Inherit some common AOSP stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Set Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
