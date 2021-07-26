4549d75411eb0d8f527801db21fcf27e53e792e0
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-07-26 16:09:04 +0800

    sdm455_64: Build libgui_vendor from source

--- device.mk
+++ device.mk
@@ -152,6 +152,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    libgui_vendor \
     vendor.qti.hardware.camera.device@1.0
 
 PRODUCT_PACKAGES += \
