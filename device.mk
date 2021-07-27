eb835087e196f3d5a648f9558e6bb87b6e670f8f
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-07-27 18:03:33 +0800

    sdm455_64: Import QTI Keymaster 4.0 from stock fw

--- device.mk
+++ device.mk
@@ -256,7 +256,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0.vendor
+    android.hardware.keymaster@4.0.vendor
 
 # Lights
 PRODUCT_PACKAGES += \
