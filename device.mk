20b2371150122924c8c55e9ea124abd9172e2cb4
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-07-29 11:37:50 +0800

    sdm455_64: Build libwifi-hal-qcom

--- device.mk
+++ device.mk
@@ -397,6 +397,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
+    libwifi-hal-qcom \
     TetheringConfigOverlay \
     WifiOverlay \
     wpa_supplicant \
