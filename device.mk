b207a799415af2a26c4125f4b8eb81e0591522c2
me-cafebabe <me_cafebabe@hotmail.com>
2022-10-07 02:48:51 +0800

    sdm455_64: Enable QCOM HW FDE

--- device.mk
+++ device.mk
@@ -218,6 +218,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# HW FDE
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
