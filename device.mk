2dd27a4b7b72de3351543750d488c3d4a238257f
me-cafebabe <me_cafebabe@hotmail.com>
2022-10-06 17:09:38 +0800

    Revert "sdm455_64: Drop QTI Keymaster 4.0"
    This reverts commit 1595b628a52d7dbed701a9b5433946be57f96e13.

--- device.mk
+++ device.mk
@@ -234,6 +234,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(DEVICE_PATH)/configs/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
