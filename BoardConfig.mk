e9d57a9edc3689885a72fd753c3d2de7934c6172
me-cafebabe <me_cafebabe@hotmail.com>
2022-10-06 16:56:30 +0800

    Revert "[TEMP] SELinux Permissive"
    This reverts commit 63a79c6923c94ab7890171fb9d463860ff95279b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,7 +91,7 @@ BOARD_KERNEL_CMDLINE := \
     console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom \
     msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 \
     service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 \
-    loop.max_part=7 androidboot.selinux=permissive
+    loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
